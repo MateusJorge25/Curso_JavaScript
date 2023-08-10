5a5ece506ead051623664c65bf676624337382a4
Mateusj2506 <mateus_jorge2002@outlook.com>
2023-08-10 17:08:34 -0300

    criação da segunda pasta e de um objeto cheio de pares de nomes e valores

---
+++ Objetos 2/app.js
@@ -0,0 +1,19 @@
+let pessoa = {
+      primerioNome: 'Mateus', 
+      segundoNome: 'Jorge',           // criação de um objeto: coleção de pares de nomes e valores
+    address: {
+      street: '111 Rua Brendolan',
+      city: 'Jaboticabal',
+      estado: 'SP'
+    }
+};
+
+
+console.log (pessoa);
+
+
+
+/* criando um objeto onde dentro do nome tem pares de nomes e valores
+e dentro do outro nome tem mais pares de nomes e valores*/
+
+
