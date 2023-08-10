96c4d187d113b0930d685620c2cfa1c56d7df40b
Mateusj2506 <mateus_jorge2002@outlook.com>
2023-08-10 15:30:32 -0300

    criação de objetos

--- objetos/app.js
+++ objetos/app.js
@@ -1,10 +1,22 @@
 let person = new Object();
 
+
 person["firstname"] = "Natalia";   // propriedade firstname funciona como referencia na memória para o objeto pessoa
-person["firstname"] = "Mateus"; 
+person["lastname"] = "Mateus"; 
 
 let firstNameProperty = "firstname";
 
 console.log(person);
 console.log(person[firstNameProperty]);
 
+console.log(person.firstname);
+console.log(person.lastname);
+
+person.address = new object();
+person.adreess.street = "111 Main St.";
+person.address.city = "New York";
+person.adress.state = "NY";
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person["address"]["state"]);
