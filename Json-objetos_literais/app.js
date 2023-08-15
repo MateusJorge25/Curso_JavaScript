232ae6c65aeb8141b6b1b9479b9f136e2d52d495
Mateusj2506 <mateus_jorge2002@outlook.com>
2023-08-15 15:24:52 -0300

    exemplo com Json

---
+++ Json-objetos_literais/app.js
@@ -0,0 +1,10 @@
+let objectLiteral = {
+    primerioname: 'Mateus',
+    eUmProgramador: true
+}
+
+console.log(JOSN.stringify(objectLiteral));
+
+let jsonValeu = JSON.parse('{"firstname": "Mateus", "eUmProgramdor": true}');
+
+console.log(jsonValue);
